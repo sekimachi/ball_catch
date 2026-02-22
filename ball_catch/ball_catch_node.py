bfb13603f71f0abcf56335e974d7bda1f550c2d2
imamura <sekimachi.287@gmail.com>
2026-02-22 18:02:33 +0900

    初期設定ファイルとノードの追加、依存関係の定義を含む新しいパッケージを作成

---
+++ ball_catch/ball_catch_node.py
@@ -0,0 +1,51 @@
+from rclpy.node import Node
+import rclpy
+from std_msgs.msg import Bool
+from std_msgs.msg import Bool
+
+from imrc_messages.msg import GeneralCommand
+from imrc_messages.msg import RobotActionProgress
+
+class BallCatch(Node):
+
+    def __init__(self):
+        super().__init__('ball_catch')
+
+        self.catch = False
+        self.goal_handle = None
+
+        # ===== Publisher =====
+        self.back_pub = self.create_publisher(Bool, 'ball_back', 10)
+        self.GC_pub = self.create_publisher(GeneralCommand,'robot_command',10)
+
+        # ===== Subscriber =====
+        self.create_subscription(Bool, 'ball_capture', self.capture_cb, 10)
+        self.create_subscription(RobotActionProgress, 'robot_progress', self.progress_cb, 10)
+        
+        
+
+    def capture_cb(self, msg: Bool):
+        self.GC_pub.publish(GeneralCommand(target="arm", param=0)) #param=0はアームを下げるっぽい
+
+
+    # ===============================
+    # robot_action_progressのコールバック。
+    # ===============================
+    def progress_cb(self, msg: RobotActionProgress):
+        self.progress_msg = msg
+
+        if msg.target == "arm" and msg.param == "down" and msg.state == "OK":
+            self.get_logger().info('アームの下降完了')
+            self.GC_pub.publish(GeneralCommand(target="arm", param=1)) #param=1はアームを上げる気がする
+        
+        elif msg.target == "arm" and msg.param == "up" and msg.state == "OK":
+            self.get_logger().info('アームの上昇完了')
+            self.back_pub.publish(Bool(data=True)) #ボールをキャッチしたことを伝える
+   
+
+
+def main():
+    rclpy.init()
+    node = BallCatch()
+    rclpy.spin(node)
+    rclpy.shutdown()
