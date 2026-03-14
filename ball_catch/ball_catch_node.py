67022352a284300e3f4b15c820869fe74295962d
imamura <sekimachi.287@gmail.com>
2026-03-14 10:23:22 +0900

    tanav2_positionコールバックの実装をコメントアウトし、キャリブレーション完了時にログメッセージを追加

--- ball_catch/ball_catch_node.py
+++ ball_catch/ball_catch_node.py
@@ -19,23 +19,22 @@ class BallCatch(Node):
         self.GC_pub = self.create_publisher(GeneralCommand,'robot_command',10)
         self.status_pub = self.create_publisher(Bool,'detect_ball_status',10)
         self.re_detect_pub = self.create_publisher(Bool,'re_detect',10)
+        self.cali_ok_pub = self.create_publisher(Bool,'cali_ok',10)
+
 
         # ===== Subscriber =====
         self.create_subscription(Bool, 'ball_capture', self.capture_cb, 10)
         self.create_subscription(RobotActionProgress, 'robot_progress', self.progress_cb, 10)
-        self.create_subscription(Bool, 'tanav2_position', self.tanav2_position_cb, 10)
+        self.create_subscription(Bool, 'ball_cali', self.cali_cb, 10)
         
-        
+
+    def cali_cb(self, msg: Bool):
+        self.GC_pub.publish(GeneralCommand(target="ball", param=5)) #param=5はキャリブレーションする
+        self.get_logger().info('キャリブレするように命令を送りました。')
 
     def capture_cb(self, msg: Bool):
         self.GC_pub.publish(GeneralCommand(target="ball", param=1)) #param=1はボールをキャッチする
         self.get_logger().info('ボールをキャッチするように命令を送りました。')
-    # ===============================
-    # tanav2_positionのコールバック。(アームを中立状態にする命令を送るためのもの)
-    # ===============================
-    # def tanav2_position_cb(self, msg: Bool):
-    #     self.GC_pub.publish(GeneralCommand(target="ball", param=4)) #param=4はアームを中立状態にする
-    #     self.get_logger().info('中立位置にするように命令を送りました。')
 
     # ===============================
     # robot_progressのコールバック。
@@ -52,8 +51,10 @@ class BallCatch(Node):
 
         elif msg.target == "ball" and msg.param == "catch"  and msg.state == "Timeout":
             pass
-
-
+        
+        elif msg.target == "calibration" and msg.param == "arm" and msg.state == "OK":
+            self.get_logger().info('キャリブレーション完了')
+            self.cali_ok_pub.publish(Bool(data=True))
 
 
 def main():
